c9f3f060d7dca05a85ff9f3e61646f75ef8195ed
Cody Njos <cody.njos@live.com>
2021-01-09 15:31:58 -0600

    Table sizing correctly monthly payroll updating after employee delete

--- client.js
+++ client.js
@@ -5,8 +5,7 @@ function readyNow() {
     $('body').on('click', '.fireButton', terminateEmployee);
     
 }
-
-let monthlyExpence = 0;
+let annualExpence = 0;
 
 function hireEmployee(){ // Adds info from input fields to DOM
     let first = $('#firstNameInput').val();
@@ -14,15 +13,15 @@ function hireEmployee(){ // Adds info from input fields to DOM
     let id = $('#idInput').val();
     let title = $('#titleInput').val();
     let salary  = $('#salaryInput').val();
-    monthlyExpence += Number(salary / 12); //Divide annual salary by 12 to get monthly expence
+    annualExpence += Number(salary); //Divide annual salary by 12 to get monthly expence
 
     $('#table').append ( //
-        `<tr id = 'test'>
+        `<tr id = 'newEmpRow' data-salary="${salary}">
         <td>${first}</td>
         <td>${last}</td>
         <td>${id}</td>
         <td>${title}</td>
-        <td>${salary}</td>
+        <td class = salary>${salary}</td>
         <td><button class = 'fireButton' type =button>Terminate Employee</button></td>
         </tr>`);
    
@@ -31,14 +30,26 @@ function hireEmployee(){ // Adds info from input fields to DOM
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#salaryInput').val('');
+    updatePayroll();
+    
+}
+function updatePayroll() {
+    let monthlyExpence = annualExpence/12;
     $('#payroll').empty() // Empty the span that holds running tally of Montly expence 
     $('#payroll').append(monthlyExpence.toFixed(2));
 }
 
-
 function terminateEmployee(){
+    console.log('start terminateEmployee');
+    //let subSalary = $(this).closest(`tr`).find(`.salary`).text();
+    let subSalary = $(this).closest(`tr`).data(`salary`);
+    console.log(subSalary);
+    annualExpence -= subSalary;
+    updatePayroll();
+    
     $(this).closest(`tr`).remove(); // Removes the table row that the button is on
+
 } 
-console.log (monthlyExpence);
+console.log (annualExpence);
 
     
