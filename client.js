2831da0a56ed0b17a322ee6eb28c811ea5822996
Cody Njos <cody.njos@live.com>
2021-01-10 13:54:03 -0600

    Fixed typos, added comments and favicon

--- client.js
+++ client.js
@@ -1,13 +1,14 @@
 $(document).ready(readyNow);
 function readyNow() {
     console.log('js & jQuery linked')
-    $('#submitButton').on('click' , hireEmployee)
-    $('body').on('click', '.fireButton', terminateEmployee);
+    $('#submitButton').on('click' , hireEmployee) // calls hire employee function when the button on the form is clicked
+    $('body').on('click', '.fireButton', terminateEmployee); //calls terminateEmployee
     
 }
-let annualExpence = 0;
+let annualExpense = 0;
 
 function hireEmployee(){ // Adds info from input fields to DOM
+    console.log('Start hireEmployee Function');
     let first = $('#firstNameInput').val();
     let last = $('#lastNameInput').val();
     let id = $('#idInput').val();
@@ -16,20 +17,20 @@ function hireEmployee(){ // Adds info from input fields to DOM
     
     // UN-COMMENT AFTER TESTING!!
     
-    /*if(first === '' || last === '' || id === ''|| title === ''|| salary === ''){
+    if(first === '' || last === '' || id === ''|| title === ''|| salary === ''){
         alert('Please Enter All Employee Information');
         return false;
-    }*/
+    }
 
-    annualExpence += Number(salary); //Adds employee salary to annual expense when they're added to the table
+    annualExpense += Number(salary); //Adds employee salary to annual expense when they're added to the table
     $('#table').append ( // uses data from the input fields to add to table on DOM
         `<tr data-salary="${salary}">
-        <td class = firstName >${first}</td>
-        <td class = lastName>${last}</td>
-        <td>${id}</td>
-        <td>${title}</td>
-        <td class = salary>${salary}</td>
-        <td><button class = 'fireButton' type =button>Terminate Employee</button></td>
+            <td class = firstName >${first}</td>
+            <td class = lastName>${last}</td>
+            <td>${id}</td>
+            <td>${title}</td>
+            <td class = salary>${salary}</td>
+            <td><button class = 'fireButton' type =button>Terminate Employee</button></td>
         </tr>`);
    
     $('#firstNameInput').val(''); // Reset input fields
@@ -37,37 +38,40 @@ function hireEmployee(){ // Adds info from input fields to DOM
     $('#idInput').val('');
     $('#titleInput').val('');
     $('#salaryInput').val('');
-    updatePayroll();
-    
+    console.log(first , last , "added to payroll")
+    updatePayroll(); // updates payroll on DOM with new employees salary added
+    console.log('$'+ salary + " added to annual payroll expense. Annual payroll expense is now $" + annualExpense )
 }
 function updatePayroll() {
-    let monthlyExpence = annualExpence/12;
+    console.log('Start updatePayroll Function')
+    let monthlyExpense = annualExpense/12;
     
-    if(monthlyExpence > 20000){ // changes text color on DOM to red if the monthly expence is over 20k
+    if(monthlyExpense > 20000){ // changes text color on DOM to red if the monthly expense is over 20k
         $('#payroll').css('color' , 'red');
     }
     else{
-        $('#payroll').css('color' , 'black'); //reverts back to black if the monthly expence drops below 20k
+        $('#payroll').css('color' , 'black'); //reverts back to black if the monthly expense drops below 20k
     }
 
-    $('#payroll').empty() // Empty the span that holds running tally of Montly expence 
-    $('#payroll').append(monthlyExpence.toFixed(2));
-    
+    $('#payroll').empty() // Empty the span that holds running tally of Montly expense 
+    $('#payroll').append(monthlyExpense.toFixed(2));
     
 }
 
 function terminateEmployee(){
-    let empFirst = $(this).closest(`tr`).find(`.firstName`).text();
+    console.log('Start terminateEmployee Function')
+    let empFirst = $(this).closest(`tr`).find(`.firstName`).text(); // pulls Name from row where button was clicked to be used in alerts
     let empLast = $(this).closest(`tr`).find(`.lastName`).text();
     //let subSalary = $(this).closest(`tr`).find(`.salary`).text(); // another way of doing what is on the line below
     let subSalary = $(this).closest(`tr`).data(`salary`);// target the closest tr with data matching `salary`
     if (confirm("Do you want to remove " + empFirst + " " + empLast + " from the payroll?")){
-        $(this).closest(`tr`).remove();
+        $(this).closest(`tr`).remove(); // removes row closest to the buttun that has been pushed
         
         
-        annualExpence -= subSalary; // subtracts fired employees salary from the annual expence
-        updatePayroll(); // updates payroll on DOM with fired employees salary subtracted
+        annualexpense -= subSalary; // subtracts removed employees salary from the annual expense
+        updatePayroll(); // updates payroll on DOM with removed employees salary subtracted
         alert(empFirst + " " + empLast + " is no longer on the payroll.") // Informs user that the employee has been removed from payroll
+        console.log(subSalary + ' removed from annual payroll expense. Annual payroll expense is now $' + annualexpense)
     }
     else{
         alert(empFirst + " " + empLast + " is still on the payroll.") // Informs that the selected employee has NOT been removed
